68f4e233b96d9d01d26a92a9e9c2b0adad8e7f87
Tasya Aditya Rukmana <tadityar@gmail.com>
2021-05-01 11:10:07 +0800

    use pdf_viewer and enable linkService

--- frontend/src/Booklet.elm
+++ frontend/src/Booklet.elm
@@ -1,8 +1,8 @@
 module Booklet exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, a, button, canvas, div, p, span, text, img)
-import Html.Attributes exposing (class, href, id, src, height, width)
+import Html exposing (Html, a, button, canvas, div, img, p, span, text)
+import Html.Attributes exposing (class, height, href, id, src, width)
 import Html.Events exposing (onClick)
 import Task
 import Time
@@ -46,11 +46,12 @@ update msg model =
         Tick newTime ->
             let
                 concertHasEnded =
-                    model.forceDisplayBanner || not
-                        ((Time.posixToMillis newTime < 1619859600000)
-                            || (Time.posixToMillis newTime > 1619938800000 && Time.posixToMillis newTime < 1619946000000)
-                            || (Time.posixToMillis newTime > 1619953200000 && Time.posixToMillis newTime < 1619960400000)
-                        )
+                    model.forceDisplayBanner
+                        || not
+                            ((Time.posixToMillis newTime < 1619859600000)
+                                || (Time.posixToMillis newTime > 1619938800000 && Time.posixToMillis newTime < 1619946000000)
+                                || (Time.posixToMillis newTime > 1619953200000 && Time.posixToMillis newTime < 1619960400000)
+                            )
             in
             ( { model | time = newTime, displaySurveyBanner = concertHasEnded }
             , Cmd.none
@@ -83,12 +84,18 @@ view model =
     let
         banner =
             if model.displaySurveyBanner then
-                div [ class "sticky top-bar" ] 
-                [ span []
-                [text "Help us improve your experience! ",
-                span [] [ a 
-                [ href "https://docs.google.com/forms/d/1rVGMwOJXjhxxdW2Z8ftcEQ3Xjn5S8rC23cpn5rB90po/edit"] [ span [] [ span [ class "link-text"] [ text "Take survey"]], img [src "assets/arrow-right.svg", width 12, height 12] []
-                ] ] ] ]
+                div [ class "sticky top-bar" ]
+                    [ span []
+                        [ text "Help us improve your experience! "
+                        , span []
+                            [ a
+                                [ href "https://docs.google.com/forms/d/1rVGMwOJXjhxxdW2Z8ftcEQ3Xjn5S8rC23cpn5rB90po/edit" ]
+                                [ span [] [ span [ class "link-text" ] [ text "Take survey" ] ]
+                                , img [ src "assets/arrow-right.svg", width 12, height 12 ] []
+                                ]
+                            ]
+                        ]
+                    ]
 
             else
                 div [] []
@@ -96,7 +103,9 @@ view model =
     div
         []
         [ banner
-        , div [ id "pdf-viewer" ] []
+        , div [ id "pdf-viewer" ]
+            [ div [ id "viewer", class "pdfViewer" ] []
+            ]
         ]
 
 
